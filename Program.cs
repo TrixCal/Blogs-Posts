77ab33422a18b0a54e942db25e45f53ffd34f134
TrixCal <traytonlarson01@gmail.com>
2022-03-28 12:49:38 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlogConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
