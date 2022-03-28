093f67179596a17b51c089fc61ce072e08ded2ca
TrixCal <traytonlarson01@gmail.com>
2022-03-28 12:54:20 -0500

    NLog

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.IO;
+using NLog.Web;
 
 namespace BlogConsole
 {
     class Program
     {
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            logger.Info("Program started");
+
             Console.WriteLine("Hello World!");
+
+            logger.Info("Program ended");
         }
     }
 }
