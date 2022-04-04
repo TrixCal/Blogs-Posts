9bd55468f1dc0e9d44cbd9005a4e461e406de64d
TrixCal <traytonlarson01@gmail.com>
2022-04-04 13:41:59 -0500

    Completed Display Blogs and Add Blog

--- Program.cs
+++ Program.cs
@@ -11,11 +11,42 @@ namespace BlogConsole
         static void Main(string[] args)
         {
             logger.Info("Program started");
-
+            var db = new BloggingContext();
             try{
+                string input = "";
+                while(input.ToLower() != "q"){
+                    Console.WriteLine("Enter your selection:");
+                    Console.WriteLine("1) Display all Blogs");
+                    Console.WriteLine("2) Create Blog");
+                    Console.WriteLine("3) Display alll Posts");
+                    Console.WriteLine("4) Create Post");
+                    Console.WriteLine("Enter q to quit");
+                    input = Console.ReadLine();
+                    Console.Clear();
+                    if(input == "1"){
+                        logger.Info("Option 1 selected");
 
+                        var query = db.Blogs.OrderBy(b => b.Name);
+                        Console.WriteLine($"{db.Blogs.Count()} Blogs returned");
+                        foreach (var item in query){
+                            Console.WriteLine(item.Name);
+                        }
+                    }else if(input == "2"){
+                        logger.Info("Option 2 selected");
+
+                        Console.Write("Enter a name for a new Blog: ");
+                        string name = Console.ReadLine();
+                        var blog = new Blog { Name = name };
+                        db.AddBlog(blog);
+                        logger.Info($"Blog added - {name}");
+                    }else if(input == "3"){
+
+                    }else if(input == "4"){
+
+                    }
+                    Console.WriteLine();
+                }
                 // Create and save a new Blog
-                var db = new BloggingContext();
                 /*Console.Write("Enter a name for a new Blog: ");
                 var name = Console.ReadLine();
 
@@ -24,14 +55,6 @@ namespace BlogConsole
                 
                 db.AddBlog(blog);
                 logger.Info("Blog added - {name}", name);*/
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query){
-                    Console.WriteLine(item.Name);
-                }
             }
             catch (Exception ex){
                 logger.Error(ex.Message);
