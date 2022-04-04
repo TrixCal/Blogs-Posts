a4209d88af9ecce07817f393efd325a721dd588c
TrixCal <traytonlarson01@gmail.com>
2022-04-04 12:55:48 -0500

    Create Program

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using NLog.Web;
 
 namespace BlogConsole
@@ -11,7 +12,29 @@ namespace BlogConsole
         {
             logger.Info("Program started");
 
-            Console.WriteLine("Hello World!");
+            try{
+
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+
+                var db = new BloggingContext();
+                db.AddBlog(blog);
+                logger.Info("Blog added - {name}", name);
+
+                // Display all Blogs from the database
+                var query = db.Blogs.OrderBy(b => b.Name);
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query){
+                    Console.WriteLine(item.Name);
+                }
+            }
+            catch (Exception ex){
+                logger.Error(ex.Message);
+            }
 
             logger.Info("Program ended");
         }
